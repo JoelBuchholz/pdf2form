02fe2eef18da51f679eb9af47e31b9a52439c911
Ciocolici <daniel.cirjeu@yahoo.com>
2024-07-03 16:16:16 +0200

    first update

--- compnents/PdfFields.tsx
+++ compnents/PdfFields.tsx
@@ -1,11 +1,17 @@
 "use client";
 import { useState } from "react";
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 const PdfFields = () => {
-  const [fieldData, setFieldData] = useState<{ name: string; type: string }[]>([]);
+  const [fieldData, setFieldData] = useState<{ name: string; type: string }[]>(
+    []
+  );
   const [formValues, setFormValues] = useState<{ [key: string]: any }>({});
   const [pdfFile, setPdfFile] = useState<File | null>(null);
-  const [fileUploadStatus, setFileUploadStatus] = useState("Bitte fügen Sie eine PDF ein");
+  const [fileUploadStatus, setFileUploadStatus] = useState(
+    "Bitte fügen Sie eine PDF ein"
+  );
 
   const fetchData = async () => {
     try {
@@ -31,7 +37,9 @@ const PdfFields = () => {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
     setPdfFile(file);
-    setFileUploadStatus(file ? "PDF bereit zum Hochladen" : "Bitte fügen Sie eine PDF ein");
+    setFileUploadStatus(
+      file ? "PDF bereit zum Hochladen" : "Bitte fügen Sie eine PDF ein"
+    );
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -52,82 +60,125 @@ const PdfFields = () => {
     }
   };
 
-    const handleFileUpload = async () => {
-      if (!pdfFile) {
-        setFileUploadStatus("Bitte fügen Sie eine PDF ein");
-        return;
-      }
-    
-      const formData = new FormData();
-      formData.append("file", pdfFile);
-    
-      try {
-        const response = await fetch("/api/file", {
-          method: "POST",
-          body: formData,
-        });
-        const data = await response.json();
-        console.log("Response from backend:", data);
-        setFileUploadStatus("Datei erfolgreich hochgeladen!");
-    
-        fetchData();
-      } catch (error) {
-        console.error("Error uploading file to backend:", error);
-        setFileUploadStatus("Fehler beim Hochladen der Datei");
-      }
-    };   
-     
-    const handleDownload = async () => {
-      try {
-        const response = await fetch("/api/download", {
-          method: "GET",
-        });
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'new_document.pdf');
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode?.removeChild(link);
-      } catch (error) {
-        console.error("Error downloading file:", error);
-      }
-    };
-  
-    return (
-      <div>
-        <h1>PDF to Form</h1>
-        <input type="file" accept=".pdf" onChange={handleFileChange} />
-        <button type="button" onClick={handleFileUpload}>PDF hochladen</button>
-        <form onSubmit={handleSubmit}>
-        <p>{fileUploadStatus}</p>
-          {fieldData.map((field, index) => (
-            <div key={index}>
-              <label>{field.name}</label>
-              {field.type === "textfield" ? (
-                <input
-                  type="text"
-                  name={field.name}
-                  value={formValues[field.name] || ""}
-                  onChange={handleChange}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  name={field.name}
-                  checked={!!formValues[field.name]}
-                  onChange={handleChange}
-                />
-              )}
-            </div>
-          ))}
-          <button type="submit">Änderungen absenden</button>
-          <button type="button" onClick={handleDownload}>PDF herunterladen</button>
-        </form>
-      </div>
-    );
-  };
-  
-  export default PdfFields;
+  const handleFileUpload = async () => {
+    if (!pdfFile) {
+      setFileUploadStatus("Bitte fügen Sie eine PDF ein");
+      return;
+    }
 
+    const formData = new FormData();
+    formData.append("file", pdfFile);
+
+    try {
+      const response = await fetch("/api/file", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log("Response from backend:", data);
+      setFileUploadStatus("Datei erfolgreich hochgeladen!");
+
+      fetchData();
+    } catch (error) {
+      console.error("Error uploading file to backend:", error);
+      setFileUploadStatus("Fehler beim Hochladen der Datei");
+    }
+  };
+
+  const handleDownload = async () => {
+    try {
+      const response = await fetch("/api/download", {
+        method: "GET",
+      });
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "new_document.pdf");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode?.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="h-screen w-screen">
+        <h1 className="font-lato text-black-87 text-24px font-bold-550 text-left mt-10 ml-4">
+          PDF to Form
+        </h1>
+        <hr />
+        <br />
+        <p className="font-lato font-bold text-left mt-10 ml-4 text-center text-red-dark">
+          {fileUploadStatus}
+        </p>
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-center">
+            <input
+              type="file"
+              accept=".pdf"
+              className="font-lato bg-green-light border-4 border-black-60 text-20px rounded-md p-1 hover:bg-gray-dark hover:border-gray-dark mr-2"
+              onChange={handleFileChange}
+            />
+            <button
+              type="button"
+              className="font-lato bg-green-light border-4 border-black-60 text-20px rounded-md p-2 hover:bg-gray-dark hover:border-gray-dark"
+              onClick={handleFileUpload}
+            >
+              PDF hochladen
+            </button>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            {fieldData.map((field, index) => (
+              <div key={index}>
+                <label>{field.name}</label>
+                {field.type === "textfield" ? (
+                  <input
+                    type="text"
+                    name={field.name}
+                    value={formValues[field.name] || ""}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    name={field.name}
+                    checked={!!formValues[field.name]}
+                    onChange={handleChange}
+                  />
+                )}
+              </div>
+            ))}
+            <br />
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="font-lato bg-green-light border-4 border-black-60 text-20px rounded-md p-2 hover:bg-gray-dark hover:border-gray-dark mr-2"
+              >
+                Änderungen absenden
+              </button>
+              <button
+                type="button"
+                className="font-lato bg-green-light border-4 border-black-60 text-20px rounded-md p-2 hover:bg-gray-dark hover:border-gray-dark"
+                onClick={handleDownload}
+              >
+                PDF herunterladen
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PdfFields;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,30 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./compnents/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lato: ["Lato", "Helvetica Neue", "Arial", "Helvetica", "sans-serif"],
+      },
+      colors: {
+        "green-light": "rgb(224, 225, 226)",
+        "black-87": "rgba(0, 0, 0, .87)",
+        "gray-dark": "#BEBEBE",
+        "red-dark": "#5B5570"
+      },
+      fontSize: {
+        "24px": "24px",
+        "20px": "20px",
+      },
+      fontWeight: {
+        "bold-550": "550",
+      },
+      borderWidth: {
+        "4": "4px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -17,4 +36,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
